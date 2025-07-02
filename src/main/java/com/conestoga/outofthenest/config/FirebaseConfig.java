149afcfdf53eb90c09f24e24eb9535fc79dd4ff8
Markzhang8617 <mzhang8617@conestogac.on.ca>
2025-07-02 16:34:03 -0400

    Add firebase environment adaption

--- src/main/java/com/conestoga/outofthenest/config/FirebaseConfig.java
+++ src/main/java/com/conestoga/outofthenest/config/FirebaseConfig.java
@@ -7,16 +7,34 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 @Configuration
 public class FirebaseConfig {
+
     @PostConstruct
     public void initFirebase() throws IOException {
-        InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
+        InputStream serviceAccount;
+
+        String firebaseJson = System.getenv("FIREBASE_CONFIG_JSON");
+
+        if (firebaseJson != null && !firebaseJson.isBlank()) {
+
+            serviceAccount = new ByteArrayInputStream(firebaseJson.getBytes(StandardCharsets.UTF_8));
+            System.out.println("[FirebaseConfig] Loaded credentials from ENV");
+        } else {
+
+            serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
+            System.out.println("[FirebaseConfig] Loaded credentials from file");
+        }
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
+
         FirebaseApp.initializeApp(options);
     }
 }
